b2cf679c6dab4d1ae9c50883d4ce148dc1f6ea59
Alize21 <pusikentut@gmail.com>
2025-05-05 20:38:07 +0700

    create ApiEndPoints file in Config

---
+++ app/Config/ApiEndpoints.php
@@ -0,0 +1,9 @@
+<?php 
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class ApiEndpoints extends BaseConfig {
+    public $komicast = 'http://localhost:3000/api/komikcast/';
+    public $komiku = 'http://localhost:3000/api/komiku/';
+}
