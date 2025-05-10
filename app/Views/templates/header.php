530daa1b7a43f9ec33f70ca48623e78093bf61e7
Alize21 <pusikentut@gmail.com>
2025-05-10 21:21:10 +0700

    fixed navbar layout in md screen and above

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -10,20 +10,24 @@
     
 <header class="w-full absolute left-0 top-0 flex items-center z-10 transition duration-500">
     <div class="container">
-        <div class="flex items-center justify-between relative px-4 py-3">
+        <div class="flex items-center justify-between relative px-4 py-3 md:px-6 md:py-4">
             <div class="hidden fixed top-0 left-0 w-[100vw] h-[100vh] bg-opacity z-[999]" id="overlay"></div>
-            <nav class="fixed w-[65%] h-[100vh] top-0 bottom-0 -left-full bg-white z-[1000] transition-all duration-500 ease-in-out" id="nav-menu">
+            <nav class="fixed w-72 h-[100vh] top-0 bottom-0 -left-full bg-white z-[1000] transition-all duration-500 ease-in-out" id="nav-menu">
                 <ul class="px-4 py-2">
-                    <li class="flex justify-between">
+                    <li class="flex justify-between md:hidden">
                         <a href="<?= base_url() ?>" class="font-semibold text-lg">Home</a>
-                        <p id="close-nav">close</p>
+                        <div class="p-2 cursor-pointer hover:bg-gray-200 hover:shadow-lg rounded-full transition duration-300" id="close-nav">
+                            <svg class="w-[18px] h-[18px] xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                        </div>
                     </li>
+                    <li>Bookmark</li>
+                    <li>Genres</li>
                     <!-- will add more options in the future -->
                 </ul>
             </nav>
 
             <div class="flex items-center">
-                <button id="hamburger">
+                <button id="hamburger" class="md:hidden">
                     <span class="w-[10px] h-[2px] bg-primary my-1 block"></span>
                     <span class="w-[15px] h-[2px] bg-primary my-1 block"></span>
                     <span class="w-[20px] h-[2px] bg-primary my-1 block"></span>
@@ -34,10 +38,20 @@
                     <a href="<?= base_url() ?>">Page Name</a>
                 </div>
             </div>
+
             
-            <div>
+            <div class="md:flex md:w-1/4 md:justify-between">
+                <nav class="hidden md:block" id="nav-menu">
+                    <ul class="flex gap-2">
+                        <li>Bookmark</li>
+                        <li>Genres</li>
+                        <!-- will add more options in the future -->
+                    </ul>
+                </nav>
                 <!-- will change this in the future -->
-                Search
+                 <div class="p-2 rounded-lg bg-gray-200 cursor-pointer">
+                     <svg class="w-[15px] h-[15px] xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                 </div>
             </div>
         </div>
 
