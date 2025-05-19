5d6a74b84ed3ef046f1c33ad2a4d647442f6f728
Alize21 <pusikentut@gmail.com>
2025-05-19 20:07:29 +0700

    add navigation up to chapter page

--- app/Views/komik/chapter.php
+++ app/Views/komik/chapter.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html>
-<html lang="css">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('assets/css/output.css') ?>">
-    <title>Document</title>
-</head>
-<body>
+<html class="scroll-smooth" id="head" lang="css">
+<body >
   
-    <section class="container mt-10 mx-auto">
+    <section class="container mt-10 mx-auto" >
         <div class="mx-auto px-6">
             <h1 class="capitalize text-2xl font-bold"><?= $chapterName ?></h1>
         </div>
@@ -49,7 +43,7 @@
             </div>
         </div>
 
-        <div class="container w-full mx-auto mb-10">
+        <div class="container w-full mx-auto mb-5">
         <?php foreach ($images as $image) : ?>
             <div class="w-fit mx-auto">
                 <img src="<?= $image ?>" alt="">
@@ -77,6 +71,14 @@
                         </div>
                     <?php endif ?>
                 </div>
+            </div>
+        </div>
+
+        <div class="container w-full mb-10">
+            <div class="mx-auto flex justify-center items-center">
+                <a class="block p-3 w-fit rounded-full ring-secondary2 hover:bg-gray-300 transition duration-300 ease-in-out md:py-4 md:px-6" href="#head">
+                    <svg class="w-[25px] h-[25px] fill-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M318 177.5c3.8-8.8 2-19-4.6-26l-136-144C172.9 2.7 166.6 0 160 0s-12.9 2.7-17.4 7.5l-136 144c-6.6 7-8.4 17.2-4.6 26S14.4 192 24 192l72 0 0 288c0 17.7 14.3 32 32 32l64 0c17.7 0 32-14.3 32-32l0-288 72 0c9.6 0 18.2-5.7 22-14.5z"/></svg>
+                </a>
             </div>
         </div>
     </section>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -31,7 +31,7 @@ if (isset($searchPage)) {
                     <li class="flex justify-between md:hidden">
                         <a href="<?= base_url() ?>" class="font-semibold text-lg">Home</a>
                         <div class="p-2 cursor-pointer hover:bg-gray-200 hover:shadow-lg rounded-full transition duration-300" id="close-nav">
-                            <svg class="w-[18px] h-[18px] xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                            <svg class="w-[18px] h-[18px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
                         </div>
                     </li>
                     <li>Bookmark</li>
@@ -63,7 +63,7 @@ if (isset($searchPage)) {
                     </ul>
                 </nav>
                  <div class="md:hidden p-2 rounded-lg bg-gray-200 cursor-pointer" id="searchButton">
-                     <svg class="w-[15px] h-[15px] xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                     <svg class="w-[15px] h-[15px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
                  </div>
 
                 <!-- > md display -->          
